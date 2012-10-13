ee6f37916f1a9fefdab92651c70cae1598b6828c
Matt Stow <matt@mattstow.com>
2012-10-13 16:44:27 +1100

    Fixed incorrect global variable. Other tweaks

--- not.required.bookmarklet.js
+++ not.required.bookmarklet.js
@@ -1,18 +1,19 @@
 /*
- * Not Required Bookmarklet v0.1
+ * Not Required Bookmarklet v0.2
  *
- * Removes all required fields to test form submissions
+ * Removes all required field attributes to test form submissions
  * 
  * Copyright (c) 2012 Matt Stow
  * http://mattstow.com
  * Licensed under the MIT license
 */
-
 (function() {
-	var form = document.forms, forms = form.length, elements;
-	for (var i=0; i<forms; i++) {
+	var form = document.forms,
+		forms = form.length,
+		elems;
+	for (var i = 0; i < forms; i++) {
 		elems = form[i].elements.length;
-		for (var x=0; x<elems; x++) {
+		for (var x = 0; x < elems; x++) {
 			form[i].elements[x].required = false;
 		}
 	}
